ae36ac08b1f17b0f455bd93f6ff6078f9d9c16f9
Febri Nugroho <febrinugroho063@gmail.com>
2019-10-29 10:27:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/responsi_034/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.responsi_034
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button_daftar.setOnClickListener(){
+            intent = Intent(this, register::class.java)
+            startActivity(intent)
+        }
+    }
+}
